e87cb7b15f4f378d6fc79f383de561bbf245d3ef
KhaDoan209 <doanvinhkha@gmaiil.com>
2022-11-22 22:30:20 +0700

    file name edit

--- app/Admin/Controller/admin-main.js
+++ app/Admin/Controller/admin-main.js
@@ -3,26 +3,26 @@ var phone = new Phone();
 var validation = new Validation();
 
 function getEle(id) {
-	return document.getElementById(id);
+   return document.getElementById(id);
 }
 
 function getPhoneList() {
-	phoneService
-		.getPhone()
-		.then(function (result) {
-			return displayPhoneList(result.data);
-		})
-		.catch(function (error) {
-			alert(`Cannot get data. 
+   phoneService
+      .getPhone()
+      .then(function (result) {
+         return displayPhoneList(result.data);
+      })
+      .catch(function (error) {
+         alert(`Cannot get data. 
           Error:${error}`);
-		});
+      });
 }
 getPhoneList();
 
 function displayPhoneList(array) {
-	var content = '';
-	array.map(function (item) {
-		content += `<tr class="animate__animated animate__fadeInRight animate__slow">
+   var content = '';
+   array.map(function (item) {
+      content += `<tr class="animate__animated animate__fadeInRight animate__slow">
         <th scope="row">${item.id}</th>
         <td>
             <div
@@ -41,205 +41,254 @@ function displayPhoneList(array) {
             <button class="btn btn-red  ml-2" onclick="removePhoneFromStore('${item.id}')"><i class="fa-solid fa-circle-minus"></i></button> 
         </td>
     </tr>`;
-	});
+   });
 
-	document.querySelector('tbody').innerHTML = content;
+   document.querySelector('tbody').innerHTML = content;
 }
 
 function addNewPhone() {
-	var name = getEle('phone-name').value;
-	var price = getEle('phone-price').value;
-	var screen = getEle('phone-screen').value;
-	var backCamera = getEle('phone-backCam').value;
-	var frontCamera = getEle('phone-frontCam').value;
-	var img = getEle('phone-img').value;
-	var desc = getEle('phone-desc').value;
-	var type = getEle('phone-type').value;
-	var isValid = true;
-	isValid &=
-		validation.checkEmpty(name, `Name can't be empty`, 'name-add-error') &&
-		validation.checkName(
-			name,
-			`Name must be less than 17 letters including white space`,
-			'name-add-error'
-		);
+   var name = getEle('phone-name').value;
+   var price = getEle('phone-price').value;
+   var screen = getEle('phone-screen').value;
+   var backCamera = getEle('phone-backCam').value;
+   var frontCamera = getEle('phone-frontCam').value;
+   var img = getEle('phone-img').value;
+   var desc = getEle('phone-desc').value;
+   var type = getEle('phone-type').value;
+   var isValid = true;
+   isValid &=
+      validation.checkEmpty(name, `Name can't be empty`, 'name-add-error') &&
+      validation.checkName(
+         name,
+         `Name must be less than 17 letters including white space`,
+         'name-add-error'
+      );
 
-	isValid &=
-		validation.checkEmpty(price, `Price can't be empty`, 'price-add-error') &&
-		validation.checkPrice(
-			price,
-			`Price must be less than 6 numbers before and 3 numbers after the comma`,
-			'price-add-error'
-		);
+   isValid &=
+      validation.checkEmpty(price, `Price can't be empty`, 'price-add-error') &&
+      validation.checkPrice(
+         price,
+         `Price must be less than 6 numbers before and 3 numbers after the comma`,
+         'price-add-error'
+      );
 
-	isValid &=
-		validation.checkEmpty(screen, `Screen can't be empty`, 'screen-add-error') &&
-		validation.checkScreen(screen, `Screen must be less than 30 letters`, 'screen-add-error');
+   isValid &=
+      validation.checkEmpty(
+         screen,
+         `Screen can't be empty`,
+         'screen-add-error'
+      ) &&
+      validation.checkScreen(
+         screen,
+         `Screen must be less than 30 letters`,
+         'screen-add-error'
+      );
 
-	isValid &=
-		validation.checkEmpty(backCamera, `Back camera can't be empty`, 'backCamera-add-error') &&
-		validation.checkCamera(
-			backCamera,
-			`Back camera must be less than 30 letters`,
-			'backCamera-add-error'
-		);
+   isValid &=
+      validation.checkEmpty(
+         backCamera,
+         `Back camera can't be empty`,
+         'backCamera-add-error'
+      ) &&
+      validation.checkCamera(
+         backCamera,
+         `Back camera must be less than 30 letters`,
+         'backCamera-add-error'
+      );
 
-	isValid &=
-		validation.checkEmpty(
-			frontCamera,
-			`Front camera can't be empty`,
-			'frontCamera-add-error'
-		) &&
-		validation.checkCamera(
-			frontCamera,
-			`Front camera must be less than 30 letters`,
-			'frontCamera-add-error'
-		);
-	isValid &= validation.checkEmpty(img, `Image link can't be empty`, 'img-add-error');
-	isValid &=
-		validation.checkEmpty(desc, `Description can't be empty`, 'description-add-error') &&
-		validation.checkDesc(
-			desc,
-			`Description must be less than 150 letters without special symbols`,
-			'description-add-error'
-		);
+   isValid &=
+      validation.checkEmpty(
+         frontCamera,
+         `Front camera can't be empty`,
+         'frontCamera-add-error'
+      ) &&
+      validation.checkCamera(
+         frontCamera,
+         `Front camera must be less than 30 letters`,
+         'frontCamera-add-error'
+      );
+   isValid &= validation.checkEmpty(
+      img,
+      `Image link can't be empty`,
+      'img-add-error'
+   );
+   isValid &=
+      validation.checkEmpty(
+         desc,
+         `Description can't be empty`,
+         'description-add-error'
+      ) &&
+      validation.checkDesc(
+         desc,
+         `Description must be less than 150 letters without special symbols`,
+         'description-add-error'
+      );
 
-	if (isValid) {
-		var newPhone = new Phone(name, price, screen, backCamera, frontCamera, img, desc, type);
-		clearInputSpace();
-		phoneService
-			.addPhone(newPhone)
-			.then((result) => {
-				alert('New product is added successfully');
-				getPhoneList();
-				resetForm();
-			})
-			.catch((error) => {
-				alert(`New product can't be added `);
-			});
-	}
+   if (isValid) {
+      var newPhone = new Phone(
+         name,
+         price,
+         screen,
+         backCamera,
+         frontCamera,
+         img,
+         desc,
+         type
+      );
+      clearInputSpace();
+      phoneService
+         .addPhone(newPhone)
+         .then((result) => {
+            alert('New product is added successfully');
+            getPhoneList();
+            resetForm();
+         })
+         .catch((error) => {
+            alert(`New product can't be added `);
+         });
+   }
 }
 
 function removePhoneFromStore(id) {
-	if (confirm('Do you want to remove the product ?')) {
-		if (confirm('Are you really sure?')) {
-			phoneService
-				.deletePhone(id)
-				.then((result) => {
-					alert(`Product removed sucessfully`);
-					getPhoneList();
-				})
-				.catch((error) => {
-					alert(`${error}. Cannot remove the product`);
-				});
-		}
-	}
+   if (confirm('Do you want to remove the product ?')) {
+      if (confirm('Are you really sure?')) {
+         phoneService
+            .deletePhone(id)
+            .then((result) => {
+               alert(`Product removed sucessfully`);
+               getPhoneList();
+            })
+            .catch((error) => {
+               alert(`${error}. Cannot remove the product`);
+            });
+      }
+   }
 }
 
 function updatePhoneInfor(id) {
-	var newName = getEle('update-name').value;
-	var newPrice = getEle('update-price').value;
-	var newScreen = getEle('update-screen').value;
-	var newBackCam = getEle('update-backCamera').value;
-	var newFrontCam = getEle('update-frontCamera').value;
-	var newImg = getEle('update-img').value;
-	var newDesc = getEle('update-desc').value;
-	var newType = getEle('update-type').value;
-	var isValid = true;
+   var newName = getEle('update-name').value;
+   var newPrice = getEle('update-price').value;
+   var newScreen = getEle('update-screen').value;
+   var newBackCam = getEle('update-backCamera').value;
+   var newFrontCam = getEle('update-frontCamera').value;
+   var newImg = getEle('update-img').value;
+   var newDesc = getEle('update-desc').value;
+   var newType = getEle('update-type').value;
+   var isValid = true;
 
-	isValid &=
-		validation.checkEmpty(newName, `Name can't be empty`, 'name-update-error') &&
-		validation.checkName(
-			newName,
-			`Name must be less than 17 letters including white space`,
-			'name-update-error'
-		);
+   isValid &=
+      validation.checkEmpty(
+         newName,
+         `Name can't be empty`,
+         'name-update-error'
+      ) &&
+      validation.checkName(
+         newName,
+         `Name must be less than 17 letters including white space`,
+         'name-update-error'
+      );
 
-	isValid &=
-		validation.checkEmpty(newPrice, `Price can't be empty`, 'price-update-error') &&
-		validation.checkPrice(
-			newPrice,
-			`Price must be less than 6 numbers before and 3 numbers after the comma`,
-			'price-update-error'
-		);
+   isValid &=
+      validation.checkEmpty(
+         newPrice,
+         `Price can't be empty`,
+         'price-update-error'
+      ) &&
+      validation.checkPrice(
+         newPrice,
+         `Price must be less than 6 numbers before and 3 numbers after the comma`,
+         'price-update-error'
+      );
 
-	isValid &=
-		validation.checkEmpty(newScreen, `Screen can't be empty`, 'screen-update-error') &&
-		validation.checkScreen(
-			newScreen,
-			`Screen must be less than 30 letters`,
-			'screen-update-error'
-		);
+   isValid &=
+      validation.checkEmpty(
+         newScreen,
+         `Screen can't be empty`,
+         'screen-update-error'
+      ) &&
+      validation.checkScreen(
+         newScreen,
+         `Screen must be less than 30 letters`,
+         'screen-update-error'
+      );
 
-	isValid &=
-		validation.checkEmpty(
-			newBackCam,
-			`Back camera can't be empty`,
-			'backCamera-update-error'
-		) &&
-		validation.checkCamera(
-			newBackCam,
-			`Back camera must be less than 30 letters without special symbols except , . ; : &`,
-			'backCamera-update-error'
-		);
+   isValid &=
+      validation.checkEmpty(
+         newBackCam,
+         `Back camera can't be empty`,
+         'backCamera-update-error'
+      ) &&
+      validation.checkCamera(
+         newBackCam,
+         `Back camera must be less than 30 letters without special symbols except , . ; : &`,
+         'backCamera-update-error'
+      );
 
-	isValid &=
-		validation.checkEmpty(
-			newFrontCam,
-			`Front camera can't be empty`,
-			'frontCamera-update-error'
-		) &&
-		validation.checkCamera(
-			newFrontCam,
-			`Front camera must be less than 30 letters without special symbols except , . ; : &`,
-			'frontCamera-update-error'
-		);
-	isValid &= validation.checkEmpty(newImg, `Image link can't be empty`, 'img-update-error');
-	isValid &=
-		validation.checkEmpty(newDesc, `Description can't be empty`, 'description-update-error') &&
-		validation.checkDesc(
-			newDesc,
-			`Description must be less than 150 letters without special symbols except , . ; : &`,
-			'description-update-error'
-		);
-	if (isValid) {
-		var updatedPhone = new Phone(
-			newName,
-			newPrice,
-			newScreen,
-			newBackCam,
-			newFrontCam,
-			newImg,
-			newDesc,
-			newType
-		);
-		if (confirm('Do you want to update the information ?')) {
-			phoneService
-				.updatePhone(id, updatedPhone)
-				.then(() => {
-					alert('Cập nhật sản phẩm thành công');
-					document.querySelector('.close').click();
-					getPhoneList();
-					viewPhoneInfor(id);
-				})
-				.catch(() => {});
-		}
-	}
+   isValid &=
+      validation.checkEmpty(
+         newFrontCam,
+         `Front camera can't be empty`,
+         'frontCamera-update-error'
+      ) &&
+      validation.checkCamera(
+         newFrontCam,
+         `Front camera must be less than 30 letters without special symbols except , . ; : &`,
+         'frontCamera-update-error'
+      );
+   isValid &= validation.checkEmpty(
+      newImg,
+      `Image link can't be empty`,
+      'img-update-error'
+   );
+   isValid &=
+      validation.checkEmpty(
+         newDesc,
+         `Description can't be empty`,
+         'description-update-error'
+      ) &&
+      validation.checkDesc(
+         newDesc,
+         `Description must be less than 150 letters without special symbols except , . ; : &`,
+         'description-update-error'
+      );
+   if (isValid) {
+      var updatedPhone = new Phone(
+         newName,
+         newPrice,
+         newScreen,
+         newBackCam,
+         newFrontCam,
+         newImg,
+         newDesc,
+         newType
+      );
+      if (confirm('Do you want to update the information ?')) {
+         phoneService
+            .updatePhone(id, updatedPhone)
+            .then(() => {
+               alert('Cập nhật sản phẩm thành công');
+               document.querySelector('.close').click();
+               getPhoneList();
+               viewPhoneInfor(id);
+            })
+            .catch(() => {});
+      }
+   }
 }
 
 function resetForm() {
-	var array = [...document.querySelectorAll('.form-row input')];
-	array.forEach((item) => {
-		item.value = '';
-	});
+   var array = [...document.querySelectorAll('.form-row input')];
+   array.forEach((item) => {
+      item.value = '';
+   });
 }
 
 function viewPhoneInfor(id) {
-	phoneService
-		.getPhoneById(id)
-		.then((result) => {
-			console.log(result.data.name);
-			var content = ` <div class="container">
+   phoneService
+      .getPhoneById(id)
+      .then((result) => {
+         console.log(result.data.name);
+         var content = ` <div class="container">
          <div class="row">
             <div class="col-5 animate__animated animate__zoomInDown animate__slow">
                <div class="img-infor">
@@ -417,48 +466,48 @@ function viewPhoneInfor(id) {
             </div>
          </div>
       </div>`;
-			document.querySelector('.phone-infor-detail').innerHTML = content;
-			document.querySelector('#nav-detail-tab').click();
-			getEle('update-name').value = result.data.name;
-			getEle('update-price').value = result.data.price;
-			getEle('update-screen').value = result.data.screen;
-			getEle('update-backCamera').value = result.data.backCamera;
-			getEle('update-frontCamera').value = result.data.frontCamera;
-			getEle('update-img').value = result.data.img;
-			getEle('update-desc').value = result.data.desc;
-			getEle('update-type').value = result.data.type;
-		})
-		.catch((error) => {
-			console.log(error);
-		});
+         document.querySelector('.phone-infor-detail').innerHTML = content;
+         document.querySelector('#nav-detail-tab').click();
+         getEle('update-name').value = result.data.name;
+         getEle('update-price').value = result.data.price;
+         getEle('update-screen').value = result.data.screen;
+         getEle('update-backCamera').value = result.data.backCamera;
+         getEle('update-frontCamera').value = result.data.frontCamera;
+         getEle('update-img').value = result.data.img;
+         getEle('update-desc').value = result.data.desc;
+         getEle('update-type').value = result.data.type;
+      })
+      .catch((error) => {
+         console.log(error);
+      });
 }
 
 function clearInputSpace() {
-	var array = document.querySelectorAll('.form-group input');
-	array.forEach((item) => {
-		item.value.replace(/\s/g, '');
-	});
+   var array = document.querySelectorAll('.form-group input');
+   array.forEach((item) => {
+      item.value.replace(/\s/g, '');
+   });
 }
 
 function searchPhone() {
-	var searchResult = [];
-	var kw = getEle('input-search').value.toLowerCase().replace(/\s/g, '');
-	phoneService
-		.getPhone()
-		.then(function (result) {
-			result.data.map((item) => {
-				var name = item.name.toLowerCase().replace(/\s/g, '');
-				var position = name.indexOf(kw);
-				if (position > -1) {
-					searchResult.push(item);
-				}
-			});
-			getEle('nav-productList-tab').click();
-			return displayPhoneList(searchResult);
-		})
-		.catch(function (error) {
-			alert(`Cannot get data.
+   var searchResult = [];
+   var kw = getEle('input-search').value.toLowerCase().replace(/\s/g, '');
+   phoneService
+      .getPhone()
+      .then(function (result) {
+         result.data.map((item) => {
+            var name = item.name.toLowerCase().replace(/\s/g, '');
+            var position = name.indexOf(kw);
+            if (position > -1) {
+               searchResult.push(item);
+            }
+         });
+         getEle('nav-productList-tab').click();
+         return displayPhoneList(searchResult);
+      })
+      .catch(function (error) {
+         alert(`Cannot get data.
 	   Error:${error}`);
-		});
+      });
 }
 getEle('input-search').onkeyup = searchPhone;
